bb8fcc0c17c401d8873aff9fa221dc9eb10083aa
mohammed <mooodo150@gmail.com>
2026-08-30 12:19:59 +0400

    feat: create ItemReportService

---
+++ src/main/java/com/example/Lost/Found/Management/System/ItemReportService.java
@@ -0,0 +1,10 @@
+package com.example.Lost.Found.Management.System;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ItemReportService {
+    private final List<ItemReport> reports =
+            new ArrayList<>();
+    private Long nextId = 1L;
+}
